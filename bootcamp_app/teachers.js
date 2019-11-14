1e2b9bfbc02e835eb8fc3c4d8ddd28b8b72c2954
sbentley2019 <sbentley000@gmail.com>
2019-11-14 00:48:13 +0000

    Create query through javascript connection to database

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,25 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+
+SELECT DISTINCT teachers.name AS teacher, cohorts.name AS cohort
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON students.cohort_id = cohorts.id
+WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(data => {
+    console.log(`${data.cohort}: ${data.teacher}`);
+  })
+})
+.catch(err => console.error('Query error', err.stack));
